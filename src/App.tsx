775bf724538f54f22d727f71a5c1538d299f099a
CodeLily <codelily98@naver.com>
2025-06-11 21:31:53 +0900

    App.tsx import Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
